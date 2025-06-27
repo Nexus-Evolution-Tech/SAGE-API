8801c3d444d8501454029118b54a9039a2062e85
igorfcfs <igorfcfs@gmail.com>
2025-06-27 19:59:39 -0300

    Consertando o conflito entre /dipositivos/status e /dispositivos/:id

--- src/routes/deviceRoutes.js
+++ src/routes/deviceRoutes.js
@@ -1,7 +1,14 @@
+const express = require('express');
 const gerarRotas = require('./genericRoutesFactory');
 const dispositivosController = require('../controllers/deviceController');
 
 const router = gerarRotas(dispositivosController, 'dispositivos');
-router.get('/dispositivos/status', dispositivosController.getStatus);
 
-module.exports = router;
+const routerExtra = express.Router();
+// 👉 Adiciona primeiro a rota específica - essa primeiro pra não entrar em conflito com /dispositivos/:id
+routerExtra.get('/dispositivos/status', dispositivosController.getStatus);
+
+// 👉 Depois monta o restante das rotas (inclui /:id, etc.)
+routerExtra.use(router);
+
+module.exports = routerExtra;
