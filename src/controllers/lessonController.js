8b7a83cd1176e2c5d8c3fa647fb60fdd839f13a4
igorfcfs <igorfcfs@gmail.com>
2025-06-30 03:16:54 -0300

    Reestruturação do Banco: Atributo Endereco e Horários

--- src/controllers/lessonController.js
+++ src/controllers/lessonController.js
@@ -1,6 +1,6 @@
 const gerarController = require('./genericControllerFactory');
 
 const tabela = 'Aula';
-const campos = ['id', 'nome', 'professor_id', 'turma_id', 'materia_id', 'inicio', 'fim', 'dia_semana'];
+const campos = ['id', 'nome', 'professor_id', 'turma_id', 'materia_id', 'inicio', 'fim', 'dia_semana', 'divisao', 'created_at', 'updated_at'];
 
 module.exports = gerarController(tabela, campos, 'aula');
--- src/controllers/schoolController.js
+++ src/controllers/schoolController.js
@@ -1,6 +1,6 @@
 const gerarController = require('./genericControllerFactory');
 
 const tabela = 'UnidadeEscolar';
-const campos = ['id', 'login', 'senha', 'cnpj', 'nome', 'numero', 'endereco', 'logo'];
+const campos = ['id', 'nome', 'numero_unidade', 'cnpj', 'login', 'senha', 'logradouro', 'numero', 'complemento', 'bairro', 'cidade', 'estado', 'cep', 'telefone_contato', 'logo'];
 
 module.exports = gerarController(tabela, campos, 'escola');
